391b613023479ff4a7b92f2ec5d9005df43d5978
sujit <sujit@192.168.10.39>
2026-09-01 19:15:30 +0530

    java practice

--- src/AbstractParticeQuestion/Question5.java
+++ src/AbstractParticeQuestion/Question5.java
@@ -1,5 +1,41 @@
 package AbstractParticeQuestion;
+import java.util.Scanner;
+public class Question5 
+{
+	    public static void main(String[] args) {
+	        // Write your code here
+	        Scanner sc = new Scanner(System.in);
+	        Employee4 ob;
+	        int choice =sc.nextInt();
+	        switch(choice)
+	        {
+	            case 1-> {
+	                ob=new Manager();
+	                ob.work();
+	            }
+	            case 2->{
+	                    ob= new Engineer();
+	                ob.work();
+	            }
+	            default -> System.out.println("Invalid Choice. Please select 1 or 2.");
+	        }
+	    }
+	}abstract class Employee4
+	{
+	    public abstract void work();
 
-public class Question5 {
+	}
+	class Manager extends Employee4
+	{
+	    public void work()
+	    {
+	        System.out.println("Manager is planning and organizing tasks.");
+	    }
+	}class Engineer extends Employee4
+	{
+	    public void work()
+	    {
+	        System.out.println("Engineer is writing code and fixing bugs.");
+	    }
 
 }
