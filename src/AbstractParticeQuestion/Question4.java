928b4f00b355a0c1920f6db579b05429ee23bc8e
sujit <sujit@192.168.28.182>
2026-09-01 19:02:34 +0530

    intial push

--- src/AbstractParticeQuestion/Question4.java
+++ src/AbstractParticeQuestion/Question4.java
@@ -1,5 +1,32 @@
 package AbstractParticeQuestion;
-
-public class Question4 {
-
+import java.util.Scanner;
+public class Question4 
+{
+	
+	    public static void main(String[] args) {
+	        // Write your code here
+	        Employee ob = new Devloper();
+	        ob.work();
+	        Employee.companyPolicy();
+	    }
+	}
+	abstract class Employee
+	{
+	    public Employee()
+	    {
+	        System.out.println("Employee constructor called");
+	    }
+	    public abstract void work();
+	    public static void companyPolicy()
+	    {
+	        System.out.println("All employees must follow company policies");
+	    }
+	}
+	class Devloper extends Employee
+	{
+	    public void work()
+	    {
+	        System.out.println("Developer is coding");
+	    }
+	
 }
